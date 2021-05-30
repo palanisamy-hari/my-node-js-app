0e9e111a968b21c086aff34c39348bfbe162d203
Hariprakash94 <30706291+Hariprakash94@users.noreply.github.com>
2021-05-30 14:12:50 -0400

    ci tests check in

---
+++ ci/ci-test/smoke-test.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require 'watir'
+require 'openssl'
+
+include RSpec::Matchers
+
+ENV['http_proxy'] = ENV['HTTP_PROXY'] = nil
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+prefs = {
+    'download' => {
+        'prompt_for_download' => false,
+        'directory_directory' => false
+    },
+    'profile' => {
+        'default_content_setting_values' => {'automatic_downloads' => 1}
+    }
+}
+
+begin
+    browser = Watir::Browser.new :firefox, url: "http://localhost:4444/", options: {prefs: prefs, options: {'useAutomationExtension' => false}}
+    browser.window.maximize
+    
+    
+    # launch and run test
+    browser.goto 'https://stark-castle-29113.herokuapp.com/' 
+    sleep 5
+    expect(browser.body.text).to eq('Hello World! - Deployed!')
+rescue RSpec::Expectations::ExpectationNotMetError => e
+    puts "Test failed"
+    puts "#{e.message}"
+    return 1
+ensure
+    browser.close unless browser.nil?
+end
+
+
+# complete test
+
+
